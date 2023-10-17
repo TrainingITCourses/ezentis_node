d40dd0eb01e629d5a47dd5635f94e8fe1a079e7f
Alberto Basalo <albertobasalo@hotmail.com>
2023-10-17 13:02:05 +0200

    feat: routes rest

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 
+const routes = require("./routes");
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World! From server!"));
+routes(app, express);
 
 app.listen(3000, () => console.log("Listening on port 3000" + new Date()));
